7662d98dbdacdffadd0533f88c713846bb5838a0
Mateo Arango <mateoarango05@gmail.com>
2024-02-25 18:19:28 -0500

    header

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+// Components
+import { HeaderComponent } from './components/header'
+
+//Data
+import dataJson from "./data/cv.json";
+
+// Type
+import { AboutData, Data } from '../type/global';
+
+function App() {
+  const data = dataJson as unknown as Data;
+  const { about }: {about: AboutData} = data;
+
+  return (
+    <div className="mx-auto h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+      <div className="lg:flex lg:justify-between lg:gap-4 h-full">
+        <HeaderComponent 
+          github={about.github}
+          linkedIn={about.linkendIn}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ type/global.d.ts
@@ -0,0 +1,11 @@
+export interface Data {
+  about: AboutData;
+}
+
+interface AboutData {
+  description: string;
+  email: string;
+  github: string;
+  linkendIn: string;
+}
+
